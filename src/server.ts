e50393cb372e5758ede676386caadc817395d9c8
Audrey Bruno <audreysiqueiracvv@gmail.com>
2024-10-14 16:04:40 -0300

    refactor: add server creation and route handling for podcast API

--- src/server.ts
+++ src/server.ts
@@ -8,11 +8,13 @@ const port = process.env.PORT || 3333;
 
 const server = http.createServer(
   async (req: http.IncomingMessage, res: http.ServerResponse) => {
-    if (req.method === "GET" && req.url === "/api/list") {
+    const [baseUrl, queryString] = req.url?.split("?") ?? ["", ""];
+
+    if (req.method === "GET" && baseUrl === "/api/list") {
       await getListEpisodes(req, res);
     }
 
-    if (req.method === "GET" && req.url === "/api/episode") {
+    if (req.method === "GET" && baseUrl === "/api/episode") {
       await getFilterEpisodes(req, res);
     }
   }
