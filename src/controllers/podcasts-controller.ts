9cc227bdc2199a8bd0299613e19e04749e212b95
Audrey Bruno <audreysiqueiracvv@gmail.com>
2024-10-14 15:43:36 -0300

    refactor: add function to filter podcast to podcast-controller

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -1,10 +1,23 @@
 import { IncomingMessage, ServerResponse } from "http";
+import { serviceFilterEpisodes } from "../services/filter-episodes-service";
 import { serviceListEpisodes } from "../services/list-episodes-services";
 
 export const getListEpisodes = async (
   req: IncomingMessage,
   res: ServerResponse
 ) => {
+  const content = await serviceListEpisodes();
+
   res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify(await serviceListEpisodes()));
+  res.end(JSON.stringify(content));
+};
+
+export const getFilterEpisodes = async (
+  req: IncomingMessage,
+  res: ServerResponse
+) => {
+  const content = await serviceFilterEpisodes("The Joe");
+
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify(content));
 };
